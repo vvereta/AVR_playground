777729b7f3d5afa5fa467220454374abd0b220cc
Vitalii Vereta <vereta.vitalii@gmail.com>
2017-07-12 17:00:32 +0300

    Add blink project
    Add simple led blinking project with timers and interrupt
    
    Signed-off-by: Vitalii Vereta <vereta.vitalii@gmail.com>

---
+++ blink/main.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int LED = 1<<5;
+
+ISR(TIMER1_OVF_vect)	
+{
+	PORTB ^= (LED);
+}
+
+void	init_led(void)
+{
+	DDRB |= LED;
+	PORTB |= LED;
+}
+
+void	init_timer(void)
+{
+	cli();
+	TCCR1B = 0;
+	TCCR1B |= (1 << CS12);	// 256 prescaler 
+	TIMSK1 |= (1 << TOIE1);	// enable timer overflow interrupt
+	sei();
+}
+
+int	main(void)
+{
+	init_led();
+	init_timer();
+	while (1)
+	;
+	return (0);
+}
